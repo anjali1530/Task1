a4b3b1e255152dcf0ee25da1d2968c7bd1acb005
anjali1530 <anjaliagrawal91716@gmail.com>
2024-06-29 18:05:53 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ window.addEventListener('scroll', scrollActive)
 
 function scrollHeader(){
     const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    
     if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
@@ -53,7 +53,7 @@ window.addEventListener('scroll', scrollHeader)
 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+
     if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
